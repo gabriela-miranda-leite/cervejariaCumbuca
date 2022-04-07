629e5a27527885688dcb905658a45c8f0412ddd6
Gabriela <gabrielamiranda11.10@gmail.com>
2022-04-07 18:13:55 -0300

    feat: create styles

---
+++ src/styles/theme.ts
@@ -0,0 +1,19 @@
+export const theme = {
+  colors: {
+    primary: '#3C4F76',
+    text: {
+      dark: '#333333',
+      light: '#ffffff',
+    },
+    background: '#FFF1E6',
+    backgroundLight: '#FEE8D6',
+    card: '#FCD2AF',
+    info: '#c1c1c1',
+  },
+  fonts: {
+    regular: 'Poppins-Regular',
+    medium: 'Poppins-Medium',
+    semiBold: 'Poppins-SemiBold',
+    bold: 'Poppins-Bold',
+  },
+};
