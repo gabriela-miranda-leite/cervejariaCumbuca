bfcbe8e5fc7ce894fc98219cd3d7386e11a129bf
Gabriela <gabrielamiranda11.10@gmail.com>
2022-04-10 19:49:51 -0300

    feat; update logo

--- src/pages/welcome/styles.ts
+++ src/pages/welcome/styles.ts
@@ -5,12 +5,14 @@ export const Container = styled.View`
   flex: 1;
   flex-direction: column;
   align-items: center;
+  justify-content: space-around;
   background-color: ${theme.colors.primary};
-  padding: 50px;
+  padding: 0 0px;
 `;
 
-export const Logo = styled.Image.attrs({resizeMode: 'contain'})`
-  flex: 1;
+export const Logo = styled.Image.attrs({resizeMode: 'cover'})`
+  width: 250px;
+  height: 250px;
   justify-content: center;
   align-items: center;
 `;
